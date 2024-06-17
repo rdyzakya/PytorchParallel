586f4803f51820a4f2d768f67d4e77e2f8d7c6ff
rdyzakya <randomzakya@gmail.com>
2024-06-17 11:55:50 +0700

    add ddp

--- 5_ddp.py
+++ 5_ddp.py
@@ -19,6 +19,7 @@ def ddp_setup(rank, world_size):
     os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = "12355"
     init_process_group(backend="nccl", rank=rank, world_size=world_size)
+    torch.cuda.set_device(rank)
 
 def main(rank, world_size):
     # DDP setup
